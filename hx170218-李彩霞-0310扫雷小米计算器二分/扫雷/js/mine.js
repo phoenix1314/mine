b5b7ec35c154f18af101d145f54cdd96225dc7db
dragonwarrior95 <dragonwarrior95@163.com>
2017-03-12 20:26:43 +0800

    初始化

--- hx170218-李彩霞-0310扫雷小米计算器二分/扫雷/js/mine.js
+++ hx170218-李彩霞-0310扫雷小米计算器二分/扫雷/js/mine.js
@@ -1,3 +1,12 @@
+
+var g_nWidth;   //格子的个数 宽 列
+var g_nHeight;  //格子的个数 高 行
+var g_nMineNum=0;  //雷的个数
+var g_nMineIndex=0; //当前雷的个数
+var g_ary = new Array(g_nHeight);  //二维数组
+var g_sMinePath = "../images/mine.gif";
+var g_bMineFlage = 9; //标记为雷
+
 /**
  * Created by dragon on 2017/3/12.
  */
@@ -45,15 +54,43 @@ function init(id,w,h) {
     }
 }
 
+/**
+ * 初始化（游戏板）
+ * @param row 行号
+ * @param col 列号
+ * @param unsure 标记为不确定
+ */
+function sweeping(row, col, unsure) {
+    if (row >= 0 && col >= 0 && row+1 < g_nHeight && col+1 < g_nWidth) {
+        if(unsure === "true") {
+            // 当前位置标记为不确定
+
+            // 先判断当前位置是否标记过，如果是则取消标记，否则添加标记（插根旗帜）
+
+        }
+        else {
+            if (g_ary[row][col] === g_bMineFlage) {
+                // 游戏结束
+            }
+            else if (g_ary[row][col] > 0 && g_ary[row][col] < 9) {
+                // 显示数字
+            }
+            else if (g_ary[row][col] == 0) {
+                sweeping();
+            }
+        }
+    }
+}
+
 
 window.onload = function () {
-    var g_nWidth;   //格子的个数 宽 列
-    var g_nHeight;  //格子的个数 高 行
-    var g_nMineNum=0;  //雷的个数
-    var g_nMineIndex=0; //当前雷的个数
-    var g_ary = new Array(g_nHeight);  //二维数组
-    var g_sMinePath = "../images/mine.gif";
-    var g_bMineFlage = true; //标记为雷
+    // var g_nWidth;   //格子的个数 宽 列
+    // var g_nHeight;  //格子的个数 高 行
+    // var g_nMineNum=0;  //雷的个数
+    // var g_nMineIndex=0; //当前雷的个数
+    // var g_ary = new Array(g_nHeight);  //二维数组
+    // var g_sMinePath = "../images/mine.gif";
+    // var g_bMineFlage = true; //标记为雷
 
     //开始游戏
     $("start").onclick = function () {
@@ -94,7 +131,7 @@ window.onload = function () {
             var x = Math.floor(Math.random()*g_nWidth);  //取随机数 列
             var y = Math.floor(Math.random()*g_nHeight); //取随机数 行
             console.log("x: "+x+" y: "+y);
-            if(g_ary[y][x] == g_sMinePath)
+            if(g_ary[y][x] === g_sMinePath)
             {
                 continue;
             }
